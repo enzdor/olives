2e86abcf17cb68da3718b8d7fae727ee524ddc68
jobutterfly <jobutterfly@tutanota.com>
2023-03-25 11:07:43 -0300

    added info to readme

---
+++ sqlc/newmock/subolives.sql
@@ -0,0 +1,5 @@
+insert into subolives (subolive_id, name) values ('music');
+insert into subolives (subolive_id, name) values ('sports');
+insert into subolives (subolive_id, name) values ('tech');
+insert into subolives (subolive_id, name) values ('games');
+insert into subolives (subolive_id, name) values ('news');
